f2758bd0dd51e6a1cd551095460988c57b872b57
mjstu3 <mjstu3@student.monash.edu>
2018-09-16 10:55:13 +1000

    Undo/Redo working

--- src/js/script.js
+++ src/js/script.js
@@ -3,14 +3,16 @@ var canvas = document.querySelector( '#canvas' );
 var context = canvas.getContext( '2d' );
 var linePoints = [];
 var toolMode = 'draw'
-var toolSize = 5;
+var toolSize = 20;
 var toolColor = '#000000'
 var canvasState = [];
+var redoBuffer = [];
 var undoButton = document.querySelector( '[data-action=undo]' );
+var redoButton = document.querySelector( '[data-action=redo]' );
 
 // Defaults
 context.strokeStyle = "#000000";
-context.lineWidth = 5;
+context.lineWidth = 20;
 context.lineJoin = "round";
 context.lineCap = "round";
 
@@ -25,32 +27,30 @@ document.querySelector( '#toolbar' ).addEventListener( 'click', selectTool );
 
 // Functions
 function clearCanvas() {
-  var result = confirm( 'Are you sure you want to delete the picture?' );
-  if ( result ) {
-    context.clearRect( 0, 0, canvas.width, canvas.height );
-    canvasState.length = 0;
-    undoButton.classList.add('disabled');
-  }
+    var result = confirm( 'Are you sure you want to delete the picture?' );
+    if ( result ) {
+        context.clearRect( 0, 0, canvas.width, canvas.height );
+        canvasState.length = 0;
+        undoButton.classList.add('disabled');
+    }
 }
 
 function draw( e ) {
-  if ( e.which === 1 || e.type === 'touchstart' || e.type === 'touchmove') {
-    window.addEventListener( 'mousemove', draw );
-    window.addEventListener( 'touchmove', draw );
-    var mouseX = e.pageX - canvas.offsetLeft;
-    console.log(e.pageX);
-    var mouseY = e.pageY - canvas.offsetTop;
-    var mouseDrag = e.type === 'mousemove';
-    if ( e.type === 'touchstart' || e.type === 'touchmove' ) {
-        console.log( e );
-        mouseX = e.touches[0].pageX - canvas.offsetLeft;
-        mouseY = e.touches[0].pageY - canvas.offsetTop;
-        mouseDrag = e.type === 'touchmove';
+    if ( e.which === 1 || e.type === 'touchstart' || e.type === 'touchmove') {
+        window.addEventListener( 'mousemove', draw );
+        window.addEventListener( 'touchmove', draw );
+        var mouseX = e.pageX - canvas.offsetLeft;
+        var mouseY = e.pageY - canvas.offsetTop;
+        var mouseDrag = e.type === 'mousemove';
+        if ( e.type === 'touchstart' || e.type === 'touchmove' ) {
+            mouseX = e.touches[0].pageX - canvas.offsetLeft;
+            mouseY = e.touches[0].pageY - canvas.offsetTop;
+            mouseDrag = e.type === 'touchmove';
+        }
+        if ( e.type === 'mousedown' || e.type === 'touchstart') saveState();
+        linePoints.push( { x: mouseX, y: mouseY, drag: mouseDrag, width: toolSize, color: toolColor } );
+        updateCanvas();
     }
-    if ( e.type === 'mousedown' || e.type === 'touchstart') saveState();
-    linePoints.push( { x: mouseX, y: mouseY, drag: mouseDrag, width: toolSize, color: toolColor } );
-    updateCanvas();
-  }
 }
 
 function highlightButton( button ) {
@@ -61,74 +61,104 @@ function highlightButton( button ) {
 }
 
 function renderLine() {
-  for ( var i = 0, length = linePoints.length; i < length; i++ ) {
-    if ( !linePoints[i].drag ) {
-      //context.stroke();
-      context.beginPath();
-      context.lineWidth = linePoints[i].width;
-      context.strokeStyle = linePoints[i].color;
-      context.moveTo( linePoints[i].x, linePoints[i].y );
-      context.lineTo( linePoints[i].x + 0.5, linePoints[i].y + 0.5 );
-    } else {
-      context.lineTo( linePoints[i].x, linePoints[i].y );
+    for ( var i = 0, length = linePoints.length; i < length; i++ ) {
+        if ( !linePoints[i].drag ) {
+            //context.stroke();
+            context.beginPath();
+            context.lineWidth = linePoints[i].width;
+            context.strokeStyle = linePoints[i].color;
+            context.moveTo( linePoints[i].x, linePoints[i].y );
+            context.lineTo( linePoints[i].x + 0.5, linePoints[i].y + 0.5 );
+        } else {
+            context.lineTo( linePoints[i].x, linePoints[i].y );
+        }
     }
-  }
 
-  if ( toolMode === 'erase' ) {
-    context.globalCompositeOperation = 'destination-out';
-  } else {
-    context.globalCompositeOperation = 'source-over';
-  }
-  context.lineWidth = toolSize;
-  context.stroke();
+    if ( toolMode === 'erase' ) {
+        context.globalCompositeOperation = 'destination-out';
+    } else {
+        context.globalCompositeOperation = 'source-over';
+    }
+    context.lineWidth = toolSize;
+    context.stroke();
 }
 
 function saveState() {
-  canvasState.unshift( context.getImageData( 0, 0, canvas.width, canvas.height ) );
-  linePoints = [];
-  if ( canvasState.length > 25 ) canvasState.length = 25;
-  undoButton.classList.remove( 'disabled' );
+    canvasState.unshift( context.getImageData( 0, 0, canvas.width, canvas.height ) );
+    console.log(canvasState[0]);
+    linePoints = [];
+    if ( canvasState.length > 25 ) canvasState.length = 25;
+    redoBuffer = [];
 }
 
 function selectTool( e ) {
-  if ( e.target === e.currentTarget ) return;
-  if ( e.target.dataset.mode ) highlightButton( e.target );
-  toolSize = e.target.dataset.size || toolSize;
-  toolMode = e.target.dataset.mode || toolMode;
-  toolColor = e.target.dataset.color || toolColor;
-  if ( e.target === undoButton ) undoState();
-  if ( e.target.dataset.action == 'delete' ) clearCanvas();
+    if ( e.target === e.currentTarget ) return;
+    if ( e.target.dataset.mode ) highlightButton( e.target );
+    toolSize = e.target.dataset.size || toolSize;
+    toolMode = e.target.dataset.mode || toolMode;
+    toolColor = e.target.dataset.color || toolColor;
+    if ( e.target === undoButton ) undoState();
+    if ( e.target === redoButton ) redoState();
+    if ( e.target.dataset.action == 'delete' ) clearCanvas();
 }
 
 function stop( e ) {
-  if ( e.which === 1 || e.type === 'touchend') {
-    window.removeEventListener( 'mousemove', draw );
-    window.removeEventListener( 'touchmove', draw );
-  }
+    if ( e.which === 1 || e.type === 'touchend') {
+        window.removeEventListener( 'mousemove', draw );
+        window.removeEventListener( 'touchmove', draw );
+    }
 }
 
 function undoState() {
-  context.putImageData( canvasState.shift(), 0, 0 );
-  if ( !canvasState.length ) undoButton.classList.add( 'disabled' );
+    if (canvasState.length > 0) {
+        saveRedo();
+        context.putImageData( canvasState.shift(), 0, 0 );
+    }
+}
+
+function saveRedo() {
+    redoBuffer.unshift( context.getImageData( 0, 0, canvas.width, canvas.height ) );
+    console.log(redoBuffer[0]);
+    linePoints = [];
+    if ( canvasState.length > 25 ) canvasState.length = 25;
+}
+
+function redoState() {
+    if (redoBuffer.length > 0) {
+        canvasState.unshift( context.getImageData( 0, 0, canvas.width, canvas.height ) );
+        context.putImageData( redoBuffer.shift(), 0, 0 );
+        linePoints = [];
+        if ( canvasState.length > 25 ) canvasState.length = 25;
+    }
 }
 
 function updateCanvas() {
-  context.clearRect( 0, 0, canvas.width, canvas.height );
-  context.putImageData( canvasState[ 0 ], 0, 0 );
-  renderLine();
+    context.clearRect( 0, 0, canvas.width, canvas.height );
+    context.putImageData( canvasState[0], 0, 0 );
+    renderLine();
 }
 
 function toggleColors() {
     document.getElementById("dropdownColor").classList.toggle("show");
-    if (document.getElementById("dropdownSize").classList.contains('show')) {
-        document.getElementById("dropdownSize").classList.toggle('show');
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show') && openDropdown.id !== 'dropdownColor') {
+            openDropdown.classList.toggle('show');
+        }
     }
 }
 
 function toggleSize() {
     document.getElementById("dropdownSize").classList.toggle("show");
-    if (document.getElementById("dropdownColor").classList.contains('show')) {
-        document.getElementById("dropdownColor").classList.toggle('show');
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show') && openDropdown.id !== 'dropdownSize') {
+            openDropdown.classList.toggle('show');
+        }
     }
 }
 
@@ -171,7 +201,7 @@ $(function(){
         showButtons: false,
         containerClassName: 'palette'
     });
-    $(".sp-picker-container").width(300);
+    $(".sp-picker-container").width(400);
 
     $("#colorpicker").spectrum("set", "black");
     
